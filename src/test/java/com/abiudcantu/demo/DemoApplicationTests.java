dca5fc9fd29420d6d735e086946beda3d73ccb59
Abiud Cantu <abiudneftali@gmail.com>
2021-01-04 17:16:21 -0600

    Initial commit

---
+++ src/test/java/com/abiudcantu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abiudcantu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
